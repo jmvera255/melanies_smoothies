6423a2084852d0668b3925a8e92b12146f30f7fd
Jessica Vera, Ph.D. <56414911+jmvera255@users.noreply.github.com>
2024-12-03 19:58:56 -0800

    Update streamlit_app.py
    update API call to use the smoothie ingredient(s) selected by the customer

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,6 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-# new section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-
 if ingredients_list:
     st.write(ingredients_list)
     st.text(ingredients_list)
@@ -35,7 +31,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.data.frame(data = smoothiefroot_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
