42906bfc43417af72f57d480bdf41165c9f3f29e
Jessica Vera, Ph.D. <56414911+jmvera255@users.noreply.github.com>
2024-12-03 19:52:47 -0800

    Update streamlit_app.py
    make API call to smoothiefroot.com, display back json content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -22,6 +23,10 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
+
+# new section to display smoothiefroot nutrition information
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
 
 if ingredients_list:
     st.write(ingredients_list)
